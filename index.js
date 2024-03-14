c4be7d30a8fcfc714010647ef1fef516eaf0a421
keithowino <designsolutions1629@gmail.com>
2024-03-14 05:04:47 +0300

    Complete lab.

---
+++ index.js
@@ -0,0 +1,72 @@
+
+// Add
+
+function add(a, b){
+  return a + b
+};
+
+let output_a = add(1, 2)
+
+console.log(output_a);
+
+// Subtract
+
+function subtract(c, d){
+  return c - d
+};
+
+let output_b = subtract(3, 4)
+
+console.log(output_b);
+
+// Multiply
+
+function multiply(e, f){
+  return e * f
+};
+
+let output_c = multiply(5, 6)
+
+console.log(output_c);
+
+// Divide
+
+function divide(g, h){
+  return g / h
+};
+
+let output_d = divide(7, 8)
+
+console.log(output_d);
+
+function increment(n){
+  return n + 1
+};
+
+let output_e = increment(10)
+
+console.log(output_e);
+
+function decrement(n){
+  return n - 1
+};
+
+let output_f = decrement(10)
+
+console.log(output_f);
+
+function makeInt(n){
+  return parseInt(n, 10)
+};
+
+let output_g = makeInt("them");
+
+console.log(output_g);
+
+function preserveDecimal(n){
+  return parseFloat(n);
+};
+
+let output_h = preserveDecimal(10.008);
+
+console.log(output_h);
